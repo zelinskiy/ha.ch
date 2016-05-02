f7bd0954a8d8d03db704981b0062d4dcf252ca64
zelinskiy <zelinskiy1917@gmail.com>
2016-05-02 22:45:42 +0300

    Finally inderstand how to add comments

--- Handler/Comment.hs
+++ Handler/Comment.hs
@@ -7,14 +7,42 @@ import Database.Persist.Types
 import Database.Persist.Sqlite
 
 
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
+
+
+
+
+
+
+entryForm :: ArticleId -> Form Comment
+entryForm articleId = renderDivs $ Comment
+    <$> pure   articleId
+    <*> areq   textField "Text" Nothing
+    
+
+
 getCommentR :: ArticleId -> Handler Html
---getCommentR = defaultLayout [whamlet| get coments|]
 getCommentR articleId = do
     article <- runDB $ get404 articleId
+    (commentWidget, enctype) <- generateFormPost (entryForm articleId)
     comments <- runDB $ selectList [CommentArticleId ==.  articleId] [Desc CommentText]
     defaultLayout $ do
         $(widgetFile "showcomments")
 
 
-postCommentR :: Handler Html
-postCommentR = defaultLayout [whamlet| post coments|]
+
+
+
+
+postCommentR :: ArticleId -> Handler Html
+postCommentR articleId = do
+    ((res,commentWidget),enctype) <- runFormPost (entryForm articleId)
+    case res of 
+         FormSuccess comment -> do 
+            commentId <- runDB $ insert comment
+            redirect $ CommentR articleId
+         _ -> defaultLayout $ do
+                setTitle "Please correct your entry form"
+
+
