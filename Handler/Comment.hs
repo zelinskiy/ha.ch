5c66cbd5ac37b1404405a7f28d273426d61ea1f6
zelinskiy <zelinskiy1917@gmail.com>
2016-05-02 20:20:08 +0300

    Basis

---
+++ Handler/Comment.hs
@@ -0,0 +1,9 @@
+module Handler.Comment where
+
+import Import
+
+getCommentR :: Handler Html
+getCommentR = error "Not yet implemented: getCommentR"
+
+postCommentR :: Handler Html
+postCommentR = error "Not yet implemented: postCommentR"
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "hach" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
